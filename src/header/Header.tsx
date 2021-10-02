d5ba53f45fb712a9f9043c0e91c757c7761eceb2
RakibRahman <md.rr.talukder@gmail.com>
2021-10-02 11:49:25 +0600

    styles updated

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="pl-10 overflow-hidden leftDiv laptop:w-3/6">
+      <div className="pb-6 pl-10 overflow-hidden leftDiv laptop:w-3/6">
         <h1 className="text-text-30 text-blue-light">What is Lorem Ipsum?</h1>
         <p>Lorem Ipsum is simply dummy text of the printing and</p>
         <button className="btn">Learn About</button>
